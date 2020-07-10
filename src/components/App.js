c68fa14ab7b57761eeefc1f3a9a28e13550e7ffb
RutSzymon <rut.szymon@gmail.com>
2020-07-10 12:58:30 +0200

    202. Using the Custom Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-
-const KEY = "AIzaSyB9ACg2NlduwdvjZ6F7sGj4Jeq5BMeJJdA";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("buildings");
 
-  // setSelectedVideo(response.data.items[0]);
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import youtube from "../apis/youtube";
 
+const KEY = "AIzaSyB9ACg2NlduwdvjZ6F7sGj4Jeq5BMeJJdA";
+
 const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     search(defaultSearchTerm);
-  }, []);
+  }, [defaultSearchTerm]);
 
   const search = async (term) => {
     const response = await youtube.get("/search", {
