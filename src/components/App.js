67dcb234e0fa130367c64c8914d2e158f2356323
RutSzymon <rut.szymon@gmail.com>
2020-07-10 12:03:57 +0200

    198. Removing a Callback

--- src/components/App.js
+++ src/components/App.js
@@ -29,10 +29,6 @@ const App = () => {
     setSelectedVideo(response.data.items[0]);
   };
 
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
@@ -42,7 +38,7 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
